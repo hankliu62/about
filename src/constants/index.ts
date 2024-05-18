7208936aa1efc1a289aacf19495449d9b65b7902
hankliu <397694072@qq.com>
2024-05-19 07:39:10 +0800

    feat(about): add constants

---
+++ src/constants/index.ts
@@ -0,0 +1,13 @@
+export const PageTitle = "刘小聪 - 个人简介";
+
+export const PageDescription = `刘小聪，一名前端程序员、React陪伴大师、Vue使用达人、Python轻度爱好者、Golang微度使用者、Github提交达人、微信阅读神作研习生、电影观阅人、前爱奇艺会员、最佳熬夜冠军提名。仅仅只是一个名字。`;
+
+export const PageKeywords = [
+  "刘小聪",
+  "前端开发",
+  "前端开发工程师",
+  "frontend",
+  "卡鲁秋",
+  "Hank",
+  "HankLiu",
+];
