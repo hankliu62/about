550fe99ff9c0b3117953f0ec0be5ac08478f4927
hankliu <397694072@qq.com>
2026-03-15 07:50:58 +0800

    feat(about): modify personality

--- src/components/SectionPersonality/index.tsx
+++ src/components/SectionPersonality/index.tsx
@@ -11,21 +11,21 @@ import { getRoutePrefix } from "@/utils/route";
 const SectionPersonality = () => {
   // 精力: 外向 | 内向
   const [energy, setEnergy] = useState<number>(71);
-  // 精神: 远见 | 现实
-  const [mind, setMind] = useState<number>(61);
-  // 判断方式: 理性 | 感受
-  const [nature, setNature] = useState<number>(71);
-  // 应对方式: 评判 | 展望
-  const [tactics, setTactics] = useState<number>(65);
-  // 身份特征: 坚决 | 起伏
-  const [identity, setIdentity] = useState<number>(51);
+  // 心智: 天马行空 | 求真务实
+  const [mind, setMind] = useState<number>(53);
+  // 天性: 理性思考 | 情感细腻
+  const [nature, setNature] = useState<number>(89);
+  // 应对方式: 运筹帷幄 | 随机应变
+  const [tactics, setTactics] = useState<number>(68);
+  // 身份特征: 自信果断 | 情绪易波动
+  const [identity, setIdentity] = useState<number>(79);
 
   const personalities = useMemo(() => {
     const enEnergyContent = "Introverted";
-    const enMindContent = "Observant";
+    const enMindContent = "Intuitive";
     const enNatureContent = "Feeling";
-    const enTacticsContent = "Judging";
-    const enIdentityContent = "Assertive";
+    const enTacticsContent = "Prospecting";
+    const enIdentityContent = "Turbulent";
 
     return [
       {
@@ -42,49 +42,49 @@ const SectionPersonality = () => {
         value: energy,
         percent: `${energy}%`,
         color: "#4298B4",
-        image: `${getRoutePrefix()}/images/personalities/16personalities_trait_${enEnergyContent.toLocaleLowerCase()}.svg`,
+        image: `${getRoutePrefix()}/images/personalities/infp/16personalities_trait_${enEnergyContent.toLocaleLowerCase()}.svg`,
         desc: "内向型的人往往更喜欢较少但深入和有意义的社交互动，通常更喜欢安静的环境。",
         link: "https://www.16personalities.com/articles/energy-introverted-vs-extraverted",
       },
       {
         type: "mind",
-        title: "精神",
+        title: "心智",
         enOptions: ["Intuitive", "Observant"],
-        options: ["远见", "现实"],
+        options: ["天马行空", "求真务实"],
         enContent: enMindContent,
-        zhContent: ["远见", "现实"][
+        zhContent: ["天马行空", "求真务实"][
           ["Intuitive", "Observant"].findIndex((item) => item === enMindContent)
         ],
         value: mind,
         percent: `${mind}%`,
         color: "#E4AE3A",
-        image: `${getRoutePrefix()}/images/personalities/16personalities_trait_${enMindContent.toLocaleLowerCase()}.svg`,
-        desc: "现实型的人务实，脚踏实地。他们往往高度关注正在发生或非常可能发生的事情。",
+        image: `${getRoutePrefix()}/images/personalities/infp/16personalities_trait_${enMindContent.toLocaleLowerCase()}.svg`,
+        desc: "天马行空型的人非常富有想象力、胸怀海纳百川，对世界始终求知若渴。他们将独创性视若珍宝，热衷于探寻那些隐藏在表象之下的深层含义，以及看似遥远却充满希望的可能性。",
         link: "https://www.16personalities.com/articles/mind-intuitive-vs-observant",
       },
       {
         type: "nature",
-        title: "判断方式",
+        title: "天性",
         enOptions: ["Thinking", "Feeling"],
-        options: ["理性", "感受"],
+        options: ["理性思考", "情感细腻"],
         enContent: enNatureContent,
-        zhContent: ["理性", "感受"][
+        zhContent: ["理性思考", "情感细腻"][
           ["Thinking", "Feeling"].findIndex((item) => item === enNatureContent)
         ],
         value: nature,
         percent: `${nature}%`,
         color: "#33A474", // rbg(51, 164, 116)
-        image: `${getRoutePrefix()}/images/personalities/16personalities_trait_${enNatureContent.toLocaleLowerCase()}.svg`,
-        desc: "感受型的人重视情感表达和敏感性。他们非常重视同理心、社会和谐及合作。",
+        image: `${getRoutePrefix()}/images/personalities/infp/16personalities_trait_${enNatureContent.toLocaleLowerCase()}.svg`,
+        desc: "情感细腻型的人对情感的表达与内心的敏感度珍视有加，他们乐于袒露自己的情感，细腻感知周遭的一切。他们将同理心、社会的和谐融洽以及人们之间的和衷共济奉为至宝，认为这些是人际交往和社会生活中不可或缺的重要元素。",
         link: "https://www.16personalities.com/articles/nature-thinking-vs-feeling",
       },
       {
         type: "tactics",
         title: "应对方式",
         enOptions: ["Judging", "Prospecting"],
-        options: ["评判", "展望"],
+        options: ["运筹帷幄", "随机应变"],
         enContent: enTacticsContent,
-        zhContent: ["评判", "展望"][
+        zhContent: ["运筹帷幄", "随机应变"][
           ["Judging", "Prospecting"].findIndex(
             (item) => item === enTacticsContent
           )
@@ -92,17 +92,17 @@ const SectionPersonality = () => {
         value: tactics,
         percent: `${tactics}%`,
         color: "#88619A", // rbg(136, 97, 154)
-        image: `${getRoutePrefix()}/images/personalities/16personalities_trait_${enTacticsContent.toLocaleLowerCase()}.svg`,
-        desc: "评判型的人果断、周到，很有条理。他们重视清晰度、可预测性和封闭性，更喜欢结构和计划，而不是自发性。",
+        image: `${getRoutePrefix()}/images/personalities/infp/16personalities_trait_${enTacticsContent.toLocaleLowerCase()}.svg`,
+        desc: "随机应变型的人极善见机行事，能够敏锐地捕捉并适应各类机会。他们大多个性灵活，特立独行，相较于安稳的状态，他们对新奇独特的事物更为青睐，甚至达到标新立异的程度。",
         link: "https://www.16personalities.com/articles/tactics-judging-vs-prospecting",
       },
       {
         type: "identity",
         title: "身份特征",
         enOptions: ["Assertive", "Turbulent"],
-        options: ["坚决", "起伏"],
+        options: ["自信果断", "情绪易波动"],
         enContent: enIdentityContent,
-        zhContent: ["坚决", "起伏"][
+        zhContent: ["自信果断", "情绪易波动"][
           ["Assertive", "Turbulent"].findIndex(
             (item) => item === enIdentityContent
           )
@@ -110,8 +110,8 @@ const SectionPersonality = () => {
         value: identity,
         percent: `${identity}%`,
         color: "#F25E62", // rbg(242, 94, 98)
-        image: `${getRoutePrefix()}/images/personalities/16personalities_trait_${enIdentityContent.toLocaleLowerCase()}.svg`,
-        desc: "坚决型的人自信、性情平和，能抵抗压力。他们拒绝过度担心，在努力实现目标时往往会自信面对。",
+        image: `${getRoutePrefix()}/images/personalities/infp/16personalities_trait_${enIdentityContent.toLocaleLowerCase()}.svg`,
+        desc: "情绪易波动型自我意识强，对压力敏感。他们在情绪上有一种紧迫感，往往以成功为导向，追求完美，渴望进步。",
         link: "https://www.16personalities.com/articles/identity-assertive-vs-turbulent",
       },
     ];
@@ -140,7 +140,7 @@ const SectionPersonality = () => {
       <div className="flex flex-col items-center justify-between space-x-[0] space-y-[10px] overflow-auto text-[#343c4b] md:flex-row md:items-stretch md:space-x-[24px] md:space-y-[0]">
         {/* 性格统计 */}
         <div className="order-1 text-[0] md:order-1">
-          <div className="relative w-[400px] overflow-hidden rounded-md bg-white px-[30px] py-[20px] before:absolute before:left-0 before:right-0 before:top-0 before:z-[2] before:h-[5px] before:bg-[#4298b4] before:content-[''] hover:shadow-md">
+          <div className="relative w-[400px] overflow-hidden rounded-md bg-white px-[30px] py-[20px] before:absolute before:left-0 before:right-0 before:top-0 before:z-[2] before:h-[5px] before:bg-[#56A278] before:content-[''] hover:shadow-md">
             <div className="relative z-[2] flex flex-col items-center justify-center">
               <header className="flex flex-col items-center justify-center">
                 <h3 className="mb-2 text-base font-normal opacity-70">
@@ -151,27 +151,28 @@ const SectionPersonality = () => {
                     className="group/more flex cursor-pointer items-center justify-between"
                     target="_blank"
                     rel="noreferrer"
-                    href="https://www.16personalities.com/ch/isfj-%E4%BA%BA%E6%A0%BC"
+                    href="https://www.16personalities.com/ch/infp-%E4%BA%BA%E6%A0%BC"
                     aria-hidden
                   >
-                    守卫者<span className="ml-1">(ISFJ-A)</span>
+                    调停者<span className="ml-1">(INFP-T)</span>
                   </a>
                 </h4>
               </header>
               <div className="mb-5">
-                <div aria-label="ISFJ的头像">
+                <div aria-label="INFP的头像">
                   <SVG
                     className="h-[200px] w-[200px] rounded-[4px] object-contain transition-all"
-                    src={`${getRoutePrefix()}/images/personalities/defender.svg`}
+                    src={`${getRoutePrefix()}/images/personalities/infp/defender.svg`}
                   />
                 </div>
               </div>
               <div className="mb-0">
                 <p className="mb-1 text-base">
-                  守卫者是非常敬业和热情的保护者，随时准备保护他们所爱的人。
+                  调停者是富有诗意、善良且无私的人，总是热衷于帮助正义事业。
                 </p>
                 <p className="mb-0 line-clamp-3 text-base">
-                  守卫者以谦逊，低调的方式帮助世界发展。这种性格类型的人勤奋敬业，对周围的人深感责任感。可以指望守卫者在截止日期前完成任务，记住生日和特殊场合，坚持传统，并为他们的亲人送去关怀和支持。但是他们很少要求认可他们所做的一切，而是更喜欢在幕后默默奉献。
+                  调停者外表或许文静谦逊，但内心世界却如熊熊烈火，充满着蓬勃的活力与炽热的激情。他们想象力丰富，创造力非凡，时常沉浸在如梦如幻的白日遐想之中，于脑海里编织出琳琅满目的故事与对话。INFP
+                  型人格以其敏感多思而闻名，无论是音乐的动人旋律、艺术的独特魅力、自然的旖旎风光，还是身边人的喜怒哀乐，都能在他们心中激起层层情感涟漪。他们情感丰富细腻，念旧怀古之情浓烈，常常精心留存那些意义非凡的纪念品，这些物品如同一束束温暖的光，照亮他们的生活，让他们的内心盈满喜悦。
                 </p>
               </div>
             </div>
@@ -227,7 +228,7 @@ const SectionPersonality = () => {
                     <div className="flex w-full flex-1 items-center justify-between gap-x-5">
                       <div
                         className={classNames(
-                          "flex w-[80px] items-center justify-start gap-x-1 whitespace-nowrap text-base",
+                          "flex w-[100px] items-center justify-start gap-x-1 whitespace-nowrap text-base",
                           {
                             "font-medium":
                               personality.enOptions[0] ===
@@ -267,7 +268,7 @@ const SectionPersonality = () => {
                       </span>
                       <div
                         className={classNames(
-                          "flex w-[80px] items-center justify-end gap-x-1 whitespace-nowrap text-base",
+                          "flex w-[100px] items-center justify-end gap-x-1 whitespace-nowrap text-base",
                           {
                             "font-medium":
                               personality.enOptions[1] ===
